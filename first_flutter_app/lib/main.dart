3067c23b544b15db57ac84d419633c7d6e72df40
RaphTech-Solutions <famouszegenee@gmail.com>
2022-04-10 01:06:59 +0100

    image is added to body via network, bgcolor changed and image centralised

--- first_flutter_app/lib/main.dart
+++ first_flutter_app/lib/main.dart
@@ -10,6 +10,18 @@ void main() => runApp(
             // To change the appBar background color
             backgroundColor: Colors.blueGrey[900],
           ),
+          //The body of the app begins here
+          body:
+              //The center widget centralise the image to the center
+              Center(
+            child: Image(
+              //image is obtained via network
+              image: NetworkImage(
+                  'https://th.bing.com/th/id/OIP.cNyVGmuOU3eoLZpSVOtGbwHaFj?pid=ImgDet&rs=1'),
+            ),
+          ),
+          //changes the bgcolor of the body
+          backgroundColor: Colors.blueGrey,
         ),
       ),
     );
